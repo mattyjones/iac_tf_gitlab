82165521d8790746a7e7653509b0debdb5bd0dfe
Matt Jones <mattjones@yieldbot.com>
2017-02-05 00:35:39 +0000

    Clean env.tf

--- env.tf
+++ env.tf
@@ -20,7 +20,7 @@
 
 variable "do_token" {
   type = "string"
-  default = "6fe5c136fdb7fd562da658595d384cc88ff39afc66732a9e9b339e49bf46e223"
+  default = ""
 }
 
 variable "pub_key" {
@@ -36,5 +36,5 @@ variable "pub_key" {
 
 variable "ssh_fingerprint" {
   type = "string"
-  default = "1d:1c:6c:bf:87:ae:1b:20:d3:d1:57:4d:7c:5c:5f:7d"
+  default = ""
 }
